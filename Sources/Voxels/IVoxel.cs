6a5e6bb9cf01f03e2f2c8d2a182d9c95020df7ce
fmathey <fmathey81@gmail.com>
2018-11-16 13:24:22 +0100

    IVoxel added

---
+++ Sources/Voxels/IVoxel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Voxalis.Toolkit.Voxels
+{
+    /// <summary>
+    /// Voxel.
+    /// </summary>
+    public interface IVoxel : IEquatable<IVoxel>
+    {
+        /// <summary>
+        /// Gets the type.
+        /// </summary>
+        /// <value>The type.</value>
+        uint Type { get; }
+
+        /// <summary>
+        /// Gets a value indicating whether this <see cref="T:Voxalis.Toolkit.Voxels.IVoxel"/> is emtpy.
+        /// </summary>
+        /// <value><c>true</c> if is emtpy; otherwise, <c>false</c>.</value>
+        bool IsEmtpy { get; }
+
+        /// <summary>
+        /// Gets a value indicating whether this <see cref="T:Voxalis.Toolkit.Voxels.IVoxel"/> is filled.
+        /// </summary>
+        /// <value><c>true</c> if is filled; otherwise, <c>false</c>.</value>
+        bool IsFilled { get; }
+    }
+}
