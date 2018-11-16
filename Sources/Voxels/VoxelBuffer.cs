71baa652859b49ba0bc0241375dfbf7ddcfe6ee0
fmathey <fmathey81@gmail.com>
2018-11-16 14:19:21 +0100

    VoxelBuffer use IVoxel

--- Sources/Voxels/VoxelBuffer.cs
+++ Sources/Voxels/VoxelBuffer.cs
@@ -2,18 +2,35 @@ using Voxalis.Toolkit.Mathematics;
 
 namespace Voxalis.Toolkit.Voxels
 {
+    /// <summary>
+    /// Voxel buffer.
+    /// </summary>
     public class VoxelBuffer
     {
+        /// <summary>
+        /// The rows.
+        /// </summary>
         public readonly Vector3i Rows;
 
-        public readonly uint[,,] Voxels;
+        /// <summary>
+        /// The voxels.
+        /// </summary>
+        public readonly IVoxel[,,] Voxels;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="T:Voxalis.Toolkit.Voxels.VoxelBuffer"/> class.
+        /// </summary>
+        /// <param name="rows">Rows.</param>
         public VoxelBuffer(Vector3i rows)
         {
             Rows = rows;
-            Voxels = Rows.ToArray3<uint>();
+            Voxels = Rows.ToArray3<IVoxel>();
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="T:Voxalis.Toolkit.Voxels.VoxelBuffer"/> class.
+        /// </summary>
+        /// <param name="rows">Rows.</param>
         public VoxelBuffer(int rows) : this(new Vector3i(rows))
         { }
     }
